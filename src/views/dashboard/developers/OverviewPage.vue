2637f7d324f8622fb2e2b6b0032e5baf597a4401
Miroslaw Shpak <bytesbay@icloud.com>
2022-12-05 10:36:19 +0200

    save

--- src/views/dashboard/developers/OverviewPage.vue
+++ src/views/dashboard/developers/OverviewPage.vue
@@ -20,12 +20,12 @@
             </div>
             <div class="d-flex">
               <div class="flex-column mr-8">
-                <div class="text-h5 font-weight-bold text-success">189</div>
+                <div class="text-h6 font-weight-bold text-success">189</div>
                 <div class="text-caption text-grey">successful</div>
               </div>
 
               <div class="flex-column">
-                <div class="text-h5 font-weight-bold text-error">99</div>
+                <div class="text-h6 font-weight-bold text-error">99</div>
                 <div class="text-caption text-grey">failed</div>
               </div>
             </div>
@@ -41,16 +41,46 @@
             <div
               class="text-body-2 text-uppercase font-weight-bold text-m-emphasis mb-2"
             >
-              api requests
+              api error distribution
             </div>
             <div class="d-flex">
               <div class="flex-column mr-8">
-                <div class="text-h5 font-weight-bold text-success">189</div>
+                <div class="text-h6 font-weight-bold text-accent">8</div>
+                <div class="text-caption text-grey">GET</div>
+              </div>
+
+              <div class="flex-column mr-8">
+                <div class="text-h6 font-weight-bold text-primary">16</div>
+                <div class="text-caption text-grey">POST</div>
+              </div>
+
+              <div class="flex-column">
+                <div class="text-h6 font-weight-bold text-error">3</div>
+                <div class="text-caption text-grey">DELETE</div>
+              </div>
+            </div>
+          </div>
+
+          <v-img :src="require('@/assets/images/chart-1.png')"></v-img>
+        </v-card>
+      </v-col>
+
+      <v-col cols="6">
+        <v-card elevation="10" variant="flat">
+          <div class="d-flex flex-column pa-6">
+            <div
+              class="text-body-2 text-uppercase font-weight-bold text-m-emphasis mb-2"
+            >
+              webhooks
+            </div>
+            <div class="d-flex">
+              <div class="flex-column mr-8">
+                <div class="text-h6 font-weight-bold text-success">189</div>
                 <div class="text-caption text-grey">successful</div>
               </div>
 
               <div class="flex-column">
-                <div class="text-h5 font-weight-bold text-error">99</div>
+                <div class="text-h6 font-weight-bold text-error">99</div>
                 <div class="text-caption text-grey">failed</div>
               </div>
             </div>
@@ -66,42 +96,22 @@
             <div
               class="text-body-2 text-uppercase font-weight-bold text-m-emphasis mb-2"
             >
-              api requests
+              webhooks response time
             </div>
             <div class="d-flex">
               <div class="flex-column mr-8">
-                <div class="text-h5 font-weight-bold text-success">189</div>
-                <div class="text-caption text-grey">successful</div>
+                <div class="text-h6 font-weight-bold text-success">17 ms</div>
+                <div class="text-caption text-grey">min</div>
               </div>
 
-              <div class="flex-column">
-                <div class="text-h5 font-weight-bold text-error">99</div>
-                <div class="text-caption text-grey">failed</div>
-              </div>
-            </div>
-          </div>
-
-          <v-img :src="require('@/assets/images/chart-1.png')"></v-img>
-        </v-card>
-      </v-col>
-
-      <v-col cols="6">
-        <v-card elevation="10" variant="flat">
-          <div class="d-flex flex-column pa-6">
-            <div
-              class="text-body-2 text-uppercase font-weight-bold text-m-emphasis mb-2"
-            >
-              api requests
-            </div>
-            <div class="d-flex">
               <div class="flex-column mr-8">
-                <div class="text-h5 font-weight-bold text-success">189</div>
-                <div class="text-caption text-grey">successful</div>
+                <div class="text-h6 font-weight-bold text-primary">62 ms</div>
+                <div class="text-caption text-grey">avg</div>
               </div>
 
               <div class="flex-column">
-                <div class="text-h5 font-weight-bold text-error">99</div>
-                <div class="text-caption text-grey">failed</div>
+                <div class="text-h6 font-weight-bold text-accent">79 ms</div>
+                <div class="text-caption text-grey">max</div>
               </div>
             </div>
           </div>
